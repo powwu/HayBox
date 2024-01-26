ed16020f7d4c38a3f18aa7d3b96cb33ed39af0e0
powwu <hazy@vivaldi.net>
2024-01-25 22:44:57 -0500

    Update config.cpp

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -74,11 +74,6 @@ void setup() {
         reset_usb_boot(0, 0);
     }
 
-    // Turn on LED to indicate firmware booted.
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
-    gpio_put(PICO_DEFAULT_LED_PIN, 1);
-
     // Create array of input sources to be used.
     static InputSource *input_sources[] = { gpio_input };
     size_t input_source_count = sizeof(input_sources) / sizeof(InputSource *);
